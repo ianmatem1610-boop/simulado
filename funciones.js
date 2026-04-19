236836ea4bc1af14a7c01162ebaa5118327b96f1
ianm1610 <ianmetem1610@gmail.com>
2026-04-19 16:14:33 -0500

    agregando la funcion de calcular

--- funciones.js
+++ funciones.js
@@ -1,1 +1,5 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos) {
+    let disponible = ingresos - egresos;
+    return disponible < 0 ? 0 : disponible;
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,3 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+let disponible = calcularDisponible(ingresos, egresos);
+document.getElementById("spnDisponible").innerText = disponible.toFixed(2);
